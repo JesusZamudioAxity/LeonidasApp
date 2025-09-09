128c2c41286a5c1f143b424bfd73949bfe0d0472
Jesús González <jesusmanuel.gonzalez@axity.com>
2025-09-09 11:11:22 -0600

    Se modificaron los testdata para que recibieran de manera correcta varias peticiones para tracking ventanilla y cerrar ASN

--- test/ASN/cerrarASN.test.js
+++ test/ASN/cerrarASN.test.js
@@ -4,29 +4,11 @@ const TestDataManager = require('../../utils/testDataManager');
 const cerrarASN = TestDataManager.getcerrarASNData();
 
  describe('ASN', () => {
-    
-      it('Flujo de ASN', async () => {
-        const {scanCode} = cerrarASN.QR;
-        console.log("code" + scanCode)
-        await CerrarASN.confirmarASN(scanCode);
-     });
+     for (const { scanCode } of cerrarASN.QR) {
+         it(`Flujo de ASN para QR: ${scanCode}`, async () => {
+             console.log("Usando QR:", scanCode);
+             await CerrarASN.confirmarASN(scanCode);
+         });
+     }
 
  });
-
-
-
-
-//  {
-//   "QR": [
-//        { "scanCode": "[>|p65715el000|q20|v003435|s13659806|ar0509202|<]" },
-//        { "scanCode": "[>|p65715el000|q20|v003435|s13659806|ar0509203|<]" },
-//         { "scanCode": "[>|p65715el000|q20|v003435|s13659806|ar0509204|<]" }
-//   ]
-// }
-
-    //  for (const { scanCode } of cerrarASN.QR) {
-    //     it(`Flujo de ASN para QR: ${scanCode}`, async () => {
-    //         console.log("Usando QR:", scanCode);
-    //         await CerrarASN.confirmarASN(scanCode);
-    //     });
-    // }
--- test/ASN/tracking.test.js
+++ test/ASN/tracking.test.js
@@ -21,49 +21,39 @@ _warningItemtSelector = 'android=new UiSelector().text("Ingresa un número de it
 
  describe('Tracking ventanilla', () => {
     
-      it('Flujo de Tracking ventanilla', async () => {
-        const { trackingNumber, scanCode } = trackingData.validItem;
+    for (const { trackingNumber, scanCode } of trackingData.validItem) {
+     it(`Flujo de Tracking ventanilla para tracking #${trackingNumber}`, async () => {
+          await scrollToText("Tracking de ventanilla");
+          await browser.pause(1500);
+
+          await TrackingPage.iniciarFlujoTracking(trackingNumber);
+
+          await FakeScan(scanCode);
+          await waitForElementToBeVisible(_itemLabelSelector, 10000);
+
+          await FakeScan(scanCode);
+          assertElementVisibleAndExists(_viewGroupSelector, 10000);
+          
+          await waitForElementToBeVisible(_itemLabelSelector, 10000);
+          await FakeScan(scanCode);
+          
+
+          await TrackingPage.continuarYSalir();
+      });
+    }
+
+
+     it('Tracking item invalido', async () => {
+     const { trackingNumber } = trackingData.invalidItem;
           await scrollToText("Tracking de ventanilla");      
           await browser.pause(1500); // Solo para observar
-          
-          //  await clickButtonInContainer("Tracking de ventanilla");
-          //  await browser.pause(1500); // Solo para observar
-          //  await enterText(_editTextSelector, trackingNumber);
-          //  await clickButtonInContainer("BUSCAR");
-          //  await waitForElementToBeVisible(_itemLabelSelector, 10000);
-          await TrackingPage.iniciarFlujoTracking(trackingNumber); 
 
-
-          console.log("Esto es el QR1: " + scanCode)
-          await FakeScan(scanCode);
-          await waitForElementToBeVisible(_itemLabelSelector, 10000);
-          console.log("Esto es el QR2: " +scanCode)
-          await FakeScan(scanCode);   
-          assertElementVisibleAndExists(_viewGroupSelector, 10000);
-          await waitForElementToBeVisible(_itemLabelSelector, 10000);
-          console.log("Esto es el QR3: " +scanCode)
-          await FakeScan(scanCode);
-          // await TrackingPage.realizarEscaneos();
-
-          //  assertElementVisibleAndExists(_siguienteButtonSelector, 10000);
-          //  await clickButtonInContainer("SIGUIENTE");
-          //  await waitForElementToBeVisible(_regresarAlMenuSelector, 10000);
-          //  await clickButtonInContainer("REGRESAR AL MENÚ");
-          await TrackingPage.continuarYSalir(); 
-     });
-
-
-       it('Tracking item invalido', async () => {
-          const { trackingNumber } = trackingData.invalidItem;
-           await scrollToText("Tracking de ventanilla");      
-           await browser.pause(1500); // Solo para observar
-
-           await clickButtonInContainer("Tracking de ventanilla");
-           await browser.pause(1500); // Solo para observar
-           await enterText(_editTextSelector, trackingNumber);
-           await clickButtonInContainer("BUSCAR");
-           assertElementVisibleAndExists(_warningItemtSelector, 10000);
-        });  
+          await clickButtonInContainer("Tracking de ventanilla");
+          await browser.pause(1500); // Solo para observar
+          await enterText(_editTextSelector, trackingNumber);
+          await clickButtonInContainer("BUSCAR");
+          assertElementVisibleAndExists(_warningItemtSelector, 10000);
+     });  
  });
 
 
@@ -92,3 +82,36 @@ _warningItemtSelector = 'android=new UiSelector().text("Ingresa un número de it
 //          await TrackingPage.continuarYSalir(); 
 //      });
 //  });
+
+////////////7
+
+// it('Flujo de Tracking ventanilla', async () => {
+//         const { trackingNumber, scanCode } = trackingData.validItem;
+//           await scrollToText("Tracking de ventanilla");      
+//           await browser.pause(1500); // Solo para observar
+          
+//           //  await clickButtonInContainer("Tracking de ventanilla");
+//           //  await browser.pause(1500); // Solo para observar
+//           //  await enterText(_editTextSelector, trackingNumber);
+//           //  await clickButtonInContainer("BUSCAR");
+//           //  await waitForElementToBeVisible(_itemLabelSelector, 10000);
+//           await TrackingPage.iniciarFlujoTracking(trackingNumber); 
+
+
+//           console.log("Esto es el QR1: " + scanCode)
+//           await FakeScan(scanCode);
+//           await waitForElementToBeVisible(_itemLabelSelector, 10000);
+//           console.log("Esto es el QR2: " +scanCode)
+//           await FakeScan(scanCode);   
+//           assertElementVisibleAndExists(_viewGroupSelector, 10000);
+//           await waitForElementToBeVisible(_itemLabelSelector, 10000);
+//           console.log("Esto es el QR3: " +scanCode)
+//           await FakeScan(scanCode);
+//           // await TrackingPage.realizarEscaneos();
+
+//           //  assertElementVisibleAndExists(_siguienteButtonSelector, 10000);
+//           //  await clickButtonInContainer("SIGUIENTE");
+//           //  await waitForElementToBeVisible(_regresarAlMenuSelector, 10000);
+//           //  await clickButtonInContainer("REGRESAR AL MENÚ");
+//           await TrackingPage.continuarYSalir(); 
+//      });
